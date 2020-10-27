a215e05e78c6025b5d0cfdf0f95be924e8d23b0e
SPKunda <sripranav.kunda@gmail.com>
2020-10-27 00:17:04 -0700

    Finished basic rev

--- ddomr.js
+++ ddomr.js
@@ -1,12 +1,20 @@
 class DomRenderer {
-    constructor() {
-       
+
+    async init(path, doc) 
+    {
+        const html = (await (await fetch(path)).text()); // html as text
+
+        window.history.pushState({"html":html,"pageTitle":"test"},"", path);
+
+        return doc.querySelector('html').innerHTML = html;
     }
 
     async change(path, doc) 
     {
         const html = (await (await fetch(path)).text()); // html as text
-        console.log(html);
-        return doc.write(html);
+
+        window.history.pushState({"html":html},"", path);
+
+        return doc.querySelector('html').innerHTML = html;
     }
 }
